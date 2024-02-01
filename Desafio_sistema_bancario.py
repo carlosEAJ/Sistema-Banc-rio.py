71d63cab51a492971e8c32cc48b8f81e9386d57e
carlosEAJ <ceajedujpg@gmail.com>
2024-01-31 22:16:13 -0300

    Desafio, otimização do sistema bancário

--- Desafio_sistema_bancario.py
+++ Desafio_sistema_bancario.py
@@ -6,52 +6,81 @@ menu = """
 => """
 
 saldo = 0
-limite = 500
 extrato = []
 numero_saques = 0
-LIMITE_SAQUES = 3
-deposito = 0
-saque_valor = 0
+logado = False
 
+
+def deposito_func(saldo):
+            deposito = int(input("informe o valor a ser depositado: "))
+            
+            if deposito < 0:
+                print("digite um valor positivo")
+            else:
+                saldo = saldo + deposito
+                print(f'Saldo atual: R${saldo:.2f}')
+                extrato.append(f'Deposito: R${deposito:.2f}')
+                return saldo
+
+def saque(numero_saques, saldo, extrato):
+            LIMITE_SAQUES = 3
+            if numero_saques < LIMITE_SAQUES:
+                saque_valor = int(input("informe o valor de saque: "))
+                if saque_valor > 500: 
+                    print("Escolha um valor até R$500")
+                else:
+                    if saque_valor > saldo:
+                        print("Saldo insuficiente")
+                    elif saque_valor < 0:
+                        print("Digite um valor positivo")
+                    else:
+                        saldo -= saque_valor
+                        numero_saques += 1  
+                        print(f'O valor sacado atualmente foi de : R${saque_valor} e seu saldo atual é de: R${saldo}, você ainda pode sacar {LIMITE_SAQUES - numero_saques}')
+                        extrato.append(f'Saque: R${saque_valor:.2f}')
+                        return saldo, numero_saques
+            else:
+                print("Você só pode sacar 3 vezes por dia")  
+
+def extrato_func(saldo, extrato):
+            print(f'Valor total do saldo: R${saldo:.2f}')
+            for item in extrato:
+                print(item)
+
+def usuario():
+    nome = input("Informe seu nome: ")
+    return nome               
+
+def senha_func(nome):
+    senha = input("Informe sua senha: ")
+    print(f'Bem vindo {nome}')
+    return senha
+
+def login():
+    nome = usuario()
+    senha_func(nome)
+
+def cpf():
+    cpf = input("Informe seu cpf: ")
+    if cpf > 11:
+        print("cpf invalido")
+    else:
+        return cpf
 
 while True:
+    
+    if not logado:
+        login()
+        logado = not logado
+        print("Bem vindo ao sistema")
     opcao = input(menu)
-    
     if opcao == 'd':
-        deposito = int(input("informe o valor a ser depositado: "))
-        
-        if deposito < 0:
-            print("digite um valor positivo")
-        else:
-            saldo = saldo + deposito
-            print(f'Saldo atual: R${saldo:.2f}')
-            extrato.append(f'Deposito: R${deposito:.2f}')
-    
+        saldo = deposito_func(saldo)
     elif opcao == 's':
-        if numero_saques < LIMITE_SAQUES:
-        
-            saque_valor = int(input("informe o valor de saque: "))
-            if saque_valor > 500: 
-                print("Escolha um valor até R$500")
-            else:
-                if saque_valor > saldo:
-                    print("Saldo insuficiente")
-                elif saque_valor < 0:
-                    print("Digite um valor positivo")
-                else:
-                    saldo -= saque_valor
-                    numero_saques += 1  
-                    print(f'O valor sacado atualmente foi de : R${saque_valor} e seu saldo atual é de: R${saldo}, você ainda pode sacar {LIMITE_SAQUES - numero_saques}')
-                    extrato.append(f'Saque: R${saque_valor:.2f}')
-        else:
-            print("Você só pode sacar 3 vezes por dia")
-        
+       saldo, numero_saques =  saque(numero_saques, saldo, extrato)
     
     elif opcao == 'e':
-        print(f'Valor total do saldo: R${saldo:.2f}')
-        for item in extrato:
-            print(item)
-        
+        extrato_func(saldo, extrato)
     
     elif opcao == 'q':
         break
